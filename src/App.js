09d8967cc6440f27822e70324af1946f8b6ed236
mrmicrowaveoven <benj@zagorski.com>
2024-07-29 23:23:48 -0500

    First round display is complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {[1,2,3,4,5,6,7,8].map(i => {
+        return match("match" + i.toString())
+      })}
     </div>
   );
 }
+
+const match = (name) =>
+    <div className={`match ${name}`}>
+        <input className="player-name player-name-top"/>
+        <div className="hor-line hor-line-top"/>
+        <div className='up-diagonal round-one joining'/>
+        <div className='down-diagonal round-one joining'/>
+        <input className="player-name player-name-bottom"/>
+        <div className='hor-line hor-line-bottom'/>
+    </div>
 
 export default App;
